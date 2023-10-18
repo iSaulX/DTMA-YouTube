d0b2c389df50d91ef8b24f0b83b66dc4373a74a6
BuildTools <weegeedabes@outlook.com>
2023-10-17 20:32:08 -0600

    Bug fix, spanish translation, and modified files for making translation easier in the future

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,37 @@
 var button = document.getElementById('button');
 let textToReplance = document.getElementById('status');
 let status = document.querySelector("all")
+var usrlang = window.navigator.userLanguage || window.navigator.language;
+usrlang = usrlang.split('-')[0];
 
-button.onclick = () => {
-    if(button.checked){
-        textToReplance.innerHTML = 'ON';
-        textToReplance.style.color = 'green';
-        chrome.storage.local.set({'isActive': true});
-    } else {
-        textToReplance.innerHTML = 'OFF';
-        textToReplance.style.color = 'red';
-        chrome.storage.local.set({'isActive': false});
+
+  //Apply the strings and CSS properties
+  const texts = strings[usrlang];
+  document.getElementById('source').innerHTML = texts.source + texts.css;
+  document.getElementById('support').innerHTML = texts.support + texts.css;
+  document.getElementById('prot').innerHTML = texts.prot;
+
+    
+  button.onclick = () => {
+      if(button.checked){
+          textToReplance.innerHTML = texts.on;
+          textToReplance.style.color = 'green';
+          chrome.storage.local.set({'isActive': true});
+      } else {
+          textToReplance.innerHTML = texts.off;
+          textToReplance.style.color = 'red';
+          chrome.storage.local.set({'isActive': false});
+      }
     }
-}
-
-
-
-chrome.storage.local.get(['isActive'], function(result) {
-    if(result.isActive){
-        button.checked = true;
-        textToReplance.innerHTML = 'ON';
-        textToReplance.style.color = 'green';
-    } else {
-        button.checked = false;
-        textToReplance.innerHTML = 'OFF';
-        textToReplance.style.color = 'red';
-    }
-});
+  
+  chrome.storage.local.get(['isActive'], function(result) {
+      if(result.isActive){
+          button.checked = true;
+          textToReplance.innerHTML = texts.on;
+          textToReplance.style.color = 'green';
+      } else {
+          button.checked = false;
+          textToReplance.innerHTML = texts.off;
+          textToReplance.style.color = 'red';
+      }
+    });
---
+++ src/scripts/strings.js
@@ -0,0 +1,19 @@
+const strings = {
+    'es': {
+      'on': 'ACTIVA',
+      'off': 'INACTIVA',
+      'source': 'Codigo fuente',
+      'support': 'Apoyanos',
+      'prot': 'La protección está: ',
+      'css' : '<div></div>'
+    },
+    'en': {
+      'on': 'ON',
+      'off': 'OFF',
+      'source': 'Source code',
+      'support': 'Support us',
+      'prot': 'The protection is: ',
+      'css' : '<div></div>'
+    }
+    //Add more languages here.
+  };
