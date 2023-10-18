c5ac12d4f1d87b3ecaf6b3eb34d0135baf6d79d3
Saul Salazar <115506411+iSaulX@users.noreply.github.com>
2023-10-18 12:50:51 -0600

    Added more languages to strings
    Addes Chinse, ukraine and more from #4

--- src/scripts/strings.js
+++ src/scripts/strings.js
@@ -15,5 +15,31 @@ const strings = {
       'prot': 'The protection is: ',
       'css' : '<div></div>'
     }
+    'uk': {
+        'on': 'УВІМКНЕНО',
+        'off': 'ВИМКНЕНО',
+        'source': 'Вихідний код',
+        'support': 'Підтримайте нас',
+        'prot': 'Захист: ',
+        'css': '<div></div>'
+    },
+    'de': {
+        'on': 'EIN',
+        'off': 'AUS',
+        'source': 'Quellcode',
+        'support': 'Unterstütze uns',
+        'prot': 'Der Schutz ist: ',
+        'css': '<div></div>'
+    },
+    'zh-CN': {
+        'on': '开',
+        'off': '关',
+        'source': '源代码',
+        'support': '支持我们',
+        'prot': '保护是: ',
+        'css': '<div></div>'
+    }
+    
     //Add more languages here.
   };
+
