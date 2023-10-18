020d8d5cbff56b831c959b2c5f8a1715ff8ba2dd
LyubomirT <terlyubomir@gmail.com>
2023-10-18 10:29:44 +0300

    Ukrainian, German, Chinese (Simplified) translations added

---
+++ README_de.md
@@ -0,0 +1,47 @@
+<h1 align='center'>Fassen Sie nicht meinen Adblock Youtube an 🚫</h1>
+
+Dieses Chrome-Erweiterung ermöglicht es Ihnen, das Youtube-Popup zu entfernen, um Ihren Adblock zu deaktivieren.
+
+#### Abschnitte
+- [Einführung ⭐](https://github.com/iSaulX/DTMA-YouTube#do-you-get-tired-of-this-popup-)
+- [Installation ⬇️](https://github.com/iSaulX/DTMA-YouTube#installation-%EF%B8%8F-)
+- [FAQ ❓](https://github.com/iSaulX/DTMA-YouTube/edit/main/README.md#faq-)
+- [Beitrag ❤️](https://github.com/iSaulX/DTMA-YouTube/edit/main/README.md#contributing-this-project-%EF%B8%8F)
+
+<h2 >Sind Sie dieses Popup müde? </h2>
+<img src='/images/popUp.png'>
+
+Diese Chrome wird das Pop-up entfernen und Ihr Video sofort abspielen, um weiter zu sehen.
+
+<h2>Installation ⬇️ </h2>
+
+[Klicken Sie hier, um herunterzuladen](https://github.com/iSaulX/DTMA-YouTube/releases/download/v1.1/source.v1.1.zip), sobald Sie es heruntergeladen haben, folgen Sie einfach den nächsten Schritten. (Wenn Sie Probleme haben, können Sie das Tutorial überprüfen):
+
+- Öffnen Sie eine neue Registerkarte in Chrome, klicken Sie auf Optionen
+- Gehen Sie zu Erweiterungen und klicken Sie auf "Erweiterungsverwaltung"
+- Klicken Sie nun auf den Entwicklermodus
+- Öffnen Sie Ihren Download-Ordner und ziehen Sie die Datei, die Sie heruntergeladen haben
+- Jetzt nur noch eine neue Chrome-Registerkarte öffnen, die Erweiterung anheften und den Schutz aktivieren
+
+Du hast es geschafft!!
+
+https://github.com/iSaulX/DTMA-YouTube/assets/115506411/e6b07689-7f8d-4b77-88b0-046af14866a1
+
+## FAQ ❓
+
+#### In welchen Browsern wird es unterstützt?
+Ich habe dieses Erweiterung bereits in Chrome, Microsft Edge versucht. Aber wenn Sie es in Ihrem Browser versuchen möchten, funktioniert es möglicherweise, wenn Ihr Browser auf Chromium basiert (Wenn Sie es in Ihrem Browser versuchen, öffnen Sie bitte eine Pull-Anfrage, um diesen Abschnitt zu ändern ❤️)
+
+#### Brauche ich einen Adblocker?
+Yep! Diese Erweiterung ist nur, um dieses Popup zu blockieren. Ich habe es mit [Adguard Extension](https://adguard.com/es/welcome.html) versucht und es funktioniert gut!
+
+#### Warum veröffentlichen Sie es nicht im Chrome Web Store?
+Einfach, Projekte wie [FastForward](https://github.com/FastForwardTeam/FastForward) (Eine Chrome-Erweiterung, um Shortener zu umgehen) werden aufgrund der "Umgehung der Zahlung" (was nicht wahr ist) auf dem Chrome Web Store heruntergefahren. Daher wird dieses Projekt möglicherweise heruntergefahren, wenn es im Chrome Web Store veröffentlicht wird.
+
+#### Kann ich dieses Projekt unterstützen?
+
+Yeah! ❤️ Jeder Beitrag zu diesem Projekt ist willkommen, um dies zu verbessern und der Gemeinschaft zu helfen. Schauen Sie sich [Contributing](https://github.com/iSaulX/DTMA-YouTube/blob/main/CONTRIBUTING.md)
+
+## Beitrag ❤️
+
+Machen Sie dieses Projekt noch besser, schauen Sie sich [Contributing](https://github.com/iSaulX/DTMA-YouTube/blob/main/CONTRIBUTING.md)
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,90 @@
-var button = document.getElementById('button');
-let textToReplance = document.getElementById('status');
-let status = document.querySelector("all")
+var button = document.getElementById("button");
+var textToReplance = document.getElementById("status");
+var languageSelect = document.getElementsByName("language")[0];
 
-button.onclick = () => {
-    if(button.checked){
-        textToReplance.innerHTML = 'ON';
-        textToReplance.style.color = 'green';
-        chrome.storage.local.set({'isActive': true});
-    } else {
-        textToReplance.innerHTML = 'OFF';
-        textToReplance.style.color = 'red';
-        chrome.storage.local.set({'isActive': false});
-    }
+// Define the text for different languages
+var textOn = {
+  en: "ON",
+  uk: "УВІМКНЕНО",
+  de: "EIN",
+  "zh-CN": "开",
+};
+
+var textOff = {
+  en: "OFF",
+  uk: "ВИМКНЕНО",
+  de: "AUS",
+  "zh-CN": "关",
+};
+
+// Define the text for different parts of the webpage
+var pageText = {
+  en: {
+    mainTitle: "DTMA YouTube",
+    protectionStatus: "The protection is",
+    sourceCodeLink: "Source Code",
+    supportLink: "Support us",
+  },
+  uk: {
+    mainTitle: "DTMA YouTube",
+    protectionStatus: "Захист",
+    sourceCodeLink: "Вихідний код",
+    supportLink: "Підтримайте нас",
+  },
+  de: {
+    mainTitle: "DTMA YouTube",
+    protectionStatus: "Der Schutz ist",
+    sourceCodeLink: "Quellcode",
+    supportLink: "Unterstütze uns",
+  },
+  "zh-CN": {
+    mainTitle: "DTMA YouTube",
+    protectionStatus: "保护是",
+    sourceCodeLink: "源代码",
+    supportLink: "支持我们",
+  },
+};
+
+var language = languageSelect.value;
+
+// Update the text according to the selected language
+function updateText() {
+  var isChecked = button.checked;
+  textToReplance.innerHTML = isChecked ? textOn[language] : textOff[language];
+  textToReplance.style.color = isChecked ? "green" : "red";
+
+  // Update text for different parts of the webpage
+  document.querySelector("h1").textContent = pageText[language].mainTitle;
+  // Update the <p> without removing the <span> inside
+  document.querySelector("p").firstChild.textContent =
+    pageText[language].protectionStatus + " ";
+  document.querySelector('a[target="_blank"]').textContent =
+    pageText[language].sourceCodeLink;
+  document.querySelector(
+    'a[href="https://github.com/iSaulX/DTMA-YouTube/CONTRIBUTING.md"]'
+  ).textContent = pageText[language].supportLink;
 }
 
+button.onclick = function () {
+  var isChecked = button.checked;
+  localStorage.setItem("isActive", isChecked);
+  updateText();
+};
 
+// Listen for changes in the language selection
+languageSelect.onchange = function () {
+  language = languageSelect.value;
+  localStorage.setItem("language", language);
+  updateText();
+};
 
-chrome.storage.local.get(['isActive'], function(result) {
-    if(result.isActive){
-        button.checked = true;
-        textToReplance.innerHTML = 'ON';
-        textToReplance.style.color = 'green';
-    } else {
-        button.checked = false;
-        textToReplance.innerHTML = 'OFF';
-        textToReplance.style.color = 'red';
-    }
-});
+if (localStorage.getItem("language")) {
+  language = localStorage.getItem("language");
+  languageSelect.value = language;
+  updateText();
+}
+
+// Get the stored value of isActive
+var isActive = localStorage.getItem("isActive");
+button.checked = isActive === "true";
+updateText();
